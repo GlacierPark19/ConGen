e23a1e975eae94fa22f552a0b8980bc2668b4c20
Michael Chapman <michael@chapman-tech.net>
2025-02-19 16:28:11 -0800

    Initial Comit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "main.hpp"
+
+
+int main(){
+
+    std::cout << "CodeGen Starting..."<<std::endl;
+    std::cout << "Version: " << CODE_GEN_VERSION << std::endl;
+
+
+    return 0;
+}
---
+++ main.hpp
@@ -0,0 +1,5 @@
+
+#include <iostream>
+#include <string>
+#define CODE_GEN_VERSION 0.1
+
