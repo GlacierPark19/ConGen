a9d3d7b5d9fecb5af3ff4052e5dd569dfd02bc47
Michael Chapman <michael@chapman-tech.net>
2025-02-20 19:25:41 -0800

    Work

---
+++ Interface/StoryHandler/storyHandler.h
@@ -0,0 +1,18 @@
+class storyHandlerClass{
+
+    public: 
+        //Setters
+
+        void updateStoryLineNumber(int storyLine){
+
+        }
+
+
+
+    private:
+
+        int currentLine, nextLine, previousLine;
+        bool isEndLine;
+        
+
+};
