e54db509375e9a6458313993edbfb372289b3c7a
Michael Chapman <michael@chapman-tech.net>
2025-02-20 19:30:35 -0800

    Working on the story handler class

--- Interface/StoryHandler/storyHandler.h
+++ Interface/StoryHandler/storyHandler.h
@@ -4,15 +4,31 @@ class storyHandlerClass{
         //Setters
 
         void updateStoryLineNumber(int storyLine){
-
+            if(storyLine = (currentLine+1)){
+                previousLine=currentLine;
+                currentLine++;
+            }else{
+                previousLine=currentLine;
+                currentLine=storyLine;
+            }
         }
 
+        void setIsEndLine(bool isThisEndLine){
+            isEndLine=isThisEndLine;
+        }
+        //getters
+        int getCurrentLine(){
+            return currentLine;
+        }
+        int getPreviousLine(){
+            return previousLine;
+        }
 
 
     private:
 
-        int currentLine, nextLine, previousLine;
+        int currentLine, previousLine;
         bool isEndLine;
-        
+
 
 };
